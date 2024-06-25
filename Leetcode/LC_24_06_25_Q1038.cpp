275af81a9e82b09a2244170708b1ddf34ef2880d
Ruggy Mango <rans2203@gmail.com>
2024-06-25 20:57:30 +0530

    Add 25 june problem Q 1038

---
+++ Leetcode/LC_24_06_25_Q1038.cpp
@@ -0,0 +1,34 @@
+// Link - https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+// Binary Search Tree to Greater Sum Tree
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+
+    void dfs(TreeNode* root) {
+        if (root == NULL) return;
+
+        dfs(root -> right);
+        root -> val += sum;
+        sum = root -> val;
+        dfs(root -> left);
+        return;
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+};
