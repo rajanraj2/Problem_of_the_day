d0cd7d5dad058a3e143eadca7159338f935351b4
Ruggy Mango <rans2203@gmail.com>
2024-08-14 20:21:35 +0530

    Add 14 aug problem Q 719 - hard : array, binary search, pointers, sorting

---
+++ Leetcode/2024_08/LC_24_08_14_Q719.cpp
@@ -0,0 +1,34 @@
+// Link - https://leetcode.com/problems/find-k-th-smallest-pair-distance/?envType=daily-question&envId=2024-08-14
+// Find K-th Smallest Pair Distance
+
+
+class Solution {
+public:
+    int smallestDistancePair(std::vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        int low = 0, high = nums.back() - nums.front();
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(nums, mid) < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+
+private:
+    int countPairs(const std::vector<int>& nums, int distance) {
+        int count = 0, left = 0;
+        for (int right = 1; right < nums.size(); ++right) {
+            while (nums[right] - nums[left] > distance) {
+                ++left;
+            }
+            count += right - left;
+        }
+        return count;
+    }
+};
