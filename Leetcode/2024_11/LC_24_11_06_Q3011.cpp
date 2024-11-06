cdba098ed6a25358f4e0455ea39a843baa1e8d52
Ruggy Mango <rans2203@gmail.com>
2024-11-06 20:13:58 +0530

    Add 06 nov problem Q 3011 - medium : array, bit, sorting

---
+++ Leetcode/2024_11/LC_24_11_06_Q3011.cpp
@@ -0,0 +1,37 @@
+// Link - https://leetcode.com/problems/find-if-array-can-be-sorted/?envType=daily-question&envId=2024-11-06
+// Find if Array Can Be Sorted
+
+
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size() - i - 1; j++) {
+                int setCount = countSetBit(nums[j]);
+                int setCount1 = countSetBit(nums[j + 1]);
+                if (setCount == setCount1 && nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
+        return isSorted(nums);
+    }
+
+private:
+    int countSetBit(int n) {
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+
+    bool isSorted(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return false;
+        }
+        return true;
+    }
+};
