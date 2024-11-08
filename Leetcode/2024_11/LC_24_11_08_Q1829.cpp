28016dc6383898992e0d4918a412778a176df0cf
Ruggy Mango <rans2203@gmail.com>
2024-11-08 09:49:44 +0530

    Add 08 nov problem Q 1829 - medium : array, bit, prefix sum

---
+++ Leetcode/2024_11/LC_24_11_08_Q1829.cpp
@@ -0,0 +1,17 @@
+// Link - https://leetcode.com/problems/maximum-xor-for-each-query/?envType=daily-question&envId=2024-11-08
+// Maximum XOR for Each Query
+
+
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size(),xorr = nums[0],maxxorr = pow(2,maximumBit)-1;
+        for(int i=1;i<n;i++)xorr ^= nums[i];
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i] = xorr^maxxorr;
+            xorr ^= nums[n-1-i];
+        }
+        return ans;
+    }
+};
