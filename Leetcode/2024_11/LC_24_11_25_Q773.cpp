17189b1352abb44dbacc53ee535f977e9bf0b104
rajanraj2 <rajanraj253545@gmail.com>
2024-11-25 10:34:29 +0530

    Add 25 nov problem Q 773 - hard : array, matrix, BFS

---
+++ Leetcode/2024_11/LC_24_11_25_Q773.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/sliding-puzzle/description/?envType=daily-question&envId=2024-11-25
+// Sliding Puzzle
+
+
+class Solution {
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {
+        vector<vector<int>> dir = {{1,3}, {0,2,4}, {1,5}, {0,4}, {1,3,5}, {2,4}};
+        string target = "123450";
+        set<string> vis;
+        queue<string> q;
+        string start = "";
+
+        for (auto row : board) {
+            for (auto col : row) {
+                start += col + '0';
+            }
+        }
+
+        q.push(start);
+        vis.insert(start);
+        int step = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                string current = q.front();
+                q.pop();
+
+                if (current == target) return step;
+                int zero = current.find('0');
+
+                for (auto move : dir[zero]) {
+                    string next = current;
+                    swap(next[move], next[zero]);
+                    if (!vis.count(next)) {
+                        vis.insert(next);
+                        q.push(next);
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+};
