20a8c01ecb29b8f73357e2da5e683e366c73953c
rajanraj2 <rajanraj253545@gmail.com>
2024-11-29 19:57:59 +0530

    Add 29 nov problem Q 2577 - hard : array, bfs, graph, heap, matrix

---
+++ Leetcode/2024_11/LC_24_11_29_Q2577.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/minimum-time-to-visit-a-cell-in-a-grid/description/?envType=daily-question&envId=2024-11-29
+// Minimum Time to Visit a Cell in a Grid
+
+
+class Solution {
+public:
+    int minimumTime(vector<vector<int>>& grid) {
+        if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        priority_queue<pair<int, pair<int, int>>, 
+                      vector<pair<int, pair<int, int>>>, 
+                      greater<pair<int, pair<int, int>>>> minHeap;
+        
+        minHeap.push({0, {0, 0}}); // time, row(x), col(y)
+        
+        vector<vector<int>> seen(rows, vector<int>(cols, 0));
+        seen[0][0] = 1;
+        
+        vector<pair<int, int>> moves = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        
+        while (!minHeap.empty()) {
+            auto curr = minHeap.top();
+            int currTime = curr.first;
+            int currRow = curr.second.first;
+            int currCol = curr.second.second;
+            
+            minHeap.pop();
+            
+            if (currRow == rows - 1 && currCol == cols - 1) 
+                return currTime;
+            
+            for (auto move : moves) {
+                int nextRow = move.first + currRow;
+                int nextCol = move.second + currCol;
+                
+                if (nextRow >= 0 && nextCol >= 0 && 
+                    nextRow < rows && nextCol < cols && 
+                    !seen[nextRow][nextCol]) {
+                    
+                    int waitTime = ((grid[nextRow][nextCol] - currTime) % 2 == 0) ? 1 : 0;
+                    int nextTime = max(currTime + 1, grid[nextRow][nextCol] + waitTime);
+                    
+                    minHeap.push({nextTime, {nextRow, nextCol}});
+                    seen[nextRow][nextCol] = 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
