ef5382947a4e114d80eb9501826e19e161d08b21
rajanraj2 <rajanraj253545@gmail.com>
2024-11-30 20:21:33 +0530

    Add 30 nov problem Q 2097 - hard : dfs, graph

---
+++ Leetcode/2024_11/LC_24_11_30_Q2097.cpp
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/valid-arrangement-of-pairs/?envType=daily-question&envId=2024-11-30
+// Valid Arrangement of Pairs
+
+
+// Hierholzer's algorithm
+
+class Solution {
+public:
+    unordered_map<int, vector<int>> adj;
+    unordered_map<int, int> deg;
+    inline void build_graph(vector<vector<int>>& pairs) {
+        for (auto& edge : pairs) {
+            int start = edge[0], end = edge[1];
+            adj[start].push_back(end);
+            deg[start]++;
+            deg[end]--;
+        }
+    }
+
+    vector<int> rpath;
+    inline void euler (int i) {
+        vector<int> stk = {i};
+        while (!stk.empty()) {
+            i = stk.back();
+            if (adj[i].empty()) {
+                rpath.push_back(i);
+                stk.pop_back();
+            }
+            else {
+                int j = adj[i].back();
+                adj[i].pop_back();
+                stk.push_back(j);
+            }
+        }
+    }
+
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        const int e = pairs.size();
+        adj.reserve(e);
+        deg.reserve(e);
+
+        build_graph(pairs);
+
+        int i0 = deg.begin()->first;
+
+        for (auto& [v, d] : deg) {
+            if (d == 1) {
+                i0 = v;
+                break;
+            }
+        }
+
+        euler(i0);
+        vector<vector<int>> result;
+        result.reserve(e);
+
+        for (int i = rpath.size() - 2; i >= 0; i--) {
+            result.push_back({rpath[i + 1], rpath[i]});
+        }
+
+        return result;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
