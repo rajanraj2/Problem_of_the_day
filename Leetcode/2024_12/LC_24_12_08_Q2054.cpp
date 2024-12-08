f23d470b276c59522767a750a5d229f54f0ee235
rajanraj2 <rajanraj253545@gmail.com>
2024-12-08 12:42:59 +0530

    Add 08 dec problem Q 2054 - medium : array, bs, dp, sorting, pq

--- Leetcode/2024_12/LC_24_12_08_Q2054.cpp
+++ Leetcode/2024_12/LC_24_12_08_Q2054.cpp
@@ -2,24 +2,48 @@
 // Two Best Non-Overlapping Events
 
 
+// Greedy approach
+
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        sort(events.begin(), events.end());
-        int maxSum = 0, maxValue = 0;
-
-        for (const auto& event : events) {
-            int start = event[0], end = event[1], value = event[2];
-            while (!pq.empty() && pq.top().first < start) {
-                maxValue = max(maxValue, pq.top().second);
-                pq.pop();
-            }
-            maxSum = max(maxSum, maxValue + value);
-            pq.push({end, value});
+        vector<vector<int>> arr;
+        for (auto& it : events) {
+            arr.push_back({it[0], 1, it[2]});
+            arr.push_back({it[1] + 1, 0, it[2]});
         }
-        
-        return maxSum;
+
+        int result = 0, maxValue = 0;
+        sort(arr.begin(), arr.end());
+        for (auto& it : arr) {
+            if (it[1]) result = max(result, it[2] + maxValue);
+            else maxValue = max(maxValue, it[2]);
+        } 
+        return result;
     }
 };
+
+
+// Heap
+
+// class Solution {
+// public:
+//     int maxTwoEvents(vector<vector<int>>& events) {
+//         int n = events.size();
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//         sort(events.begin(), events.end());
+//         int maxSum = 0, maxValue = 0;
+
+//         for (const auto& event : events) {
+//             int start = event[0], end = event[1], value = event[2];
+//             while (!pq.empty() && pq.top().first < start) {
+//                 maxValue = max(maxValue, pq.top().second);
+//                 pq.pop();
+//             }
+//             maxSum = max(maxSum, maxValue + value);
+//             pq.push({end, value});
+//         }
+
+//         return maxSum;
+//     }
+// };
