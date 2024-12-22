91c6ee22531732dba8effc8e25563447ccd323ce
rajanraj2 <rajanraj253545@gmail.com>
2024-12-22 22:39:33 +0530

    Add 22 dec problem Q 2940 - hard : array, bs, stack, tree, heap

---
+++ Leetcode/2024_12/LC_24_12_22_Q2940.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet/?envType=daily-question&envId=2024-12-22
+// Find the Building Where Alice and Bob Can Meet
+
+
+class Solution {
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        int n = heights.size(), q = queries.size();
+        vector<int> result(q, -1);
+        vector<vector<pair<int, int>>> deferred(n);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+        for (int i = 0; i < q; ++i) {
+            int a = queries[i][0], b = queries[i][1];
+            if (a > b) swap(a, b);
+            if (a == b || heights[a] < heights[b]) result[i] = b;
+            else deferred[b].emplace_back(heights[a], i);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (auto& query : deferred[i]) pq.push(query);
+            while (!pq.empty() && pq.top().first < heights[i]) {
+                result[pq.top().second] = i;
+                pq.pop();
+            }
+        }
+
+        return result;
+    }
+};
