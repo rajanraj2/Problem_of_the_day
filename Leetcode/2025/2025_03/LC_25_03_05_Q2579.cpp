f3123910aa3c7f989de90261e1ce8a94e13bad15
rajanraj2 <rajanraj253545@gmail.com>
2025-03-05 05:13:35 +0000

    Add 05 march problem Q 2579 - medium : math

---
+++ Leetcode/2025/2025_03/LC_25_03_05_Q2579.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/count-total-number-of-colored-cells/description/?envType=daily-question&envId=2025-03-05
+// 2579. Count Total Number of Colored Cells
+
+
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long result = 1;
+        if (n <= 1) return result;
+        result += LLONG((n - 1) * (2 * 4 + (n - 2) * 4) / 2);
+        return result;
+    }
+};
